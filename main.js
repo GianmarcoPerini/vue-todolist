14676ae0a76cea7a725b8943828e5227402f11b9
GianmarcoPerini <g.marco.perini@gmail.com>
2021-09-16 18:00:47 +0200

    rev_1

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 let app = new Vue({
     el: '#root',
     data:{
-        toDo:['prova1','prova2','prova3'],
+        toDo:[],
         things: '',
         great: 'Non hai impegni',
         under: 'line-through'
@@ -11,12 +11,22 @@ let app = new Vue({
     methods: {
         add: function(x){
             if(this.things.trim().length > 0 ){
-            this.toDo.push(x)
+                obj = {
+                    things: x,
+                    active: false,
+                }
+            this.toDo.push(obj)
             this.things = ''
             } 
         },
+        
         remove: function(x){
             this.toDo.splice(x, 1)
         },
+
+        isActive: function(){
+            this.active = true
+            console.log(this.active);
+        }
     },
 })
